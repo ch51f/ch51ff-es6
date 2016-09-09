898d8d3212bfd45c50cbf035123cbf22f3d837e6
ch51ff <371476909@qq.com>
2016-09-09 11:42:16 +0800

    bmap control

--- public/js/bmap/control.js
+++ public/js/bmap/control.js
@@ -1,17 +1,88 @@
 (function($) {
 	var map = new BMap.Map("allmap");
 
-	var map_type_control = new BMap.MapTypeControl();
-	var overview_map_control = new BMap.OverviewMapControl();
-	var navigation_control = new BMap.NavigationControl();
-	var copyright_control = new BMap.CopyrightControl();
-	var scale_control = new BMap.ScaleControl();
-	var geolocation_control = new BMap.GeolocationControl();
+	var anchor = [BMAP_ANCHOR_TOP_LEFT, BMAP_ANCHOR_TOP_RIGHT, BMAP_ANCHOR_BOTTOM_LEFT, BMAP_ANCHOR_BOTTOM_RIGHT];
+	var length_unit = [BMAP_UNIT_METRIC, BMAP_UNIT_IMPERIAL];
+	var navigation_control_type = [BMAP_NAVIGATION_CONTROL_LARGE, BMAP_NAVIGATION_CONTROL_SMALL, BMAP_NAVIGATION_CONTROL_PAN, BMAP_NAVIGATION_CONTROL_ZOOM];
+	var map_type_control_type = [BMAP_MAPTYPE_CONTROL_HORIZONTAL, BMAP_MAPTYPE_CONTROL_DROPDOWN, BMAP_MAPTYPE_CONTROL_MAP];
+	var status_code = [BMAP_STATUS_PERMISSION_DENIED, BMAP_STATUS_SERVICE_UNAVAILABLE, BMAP_STATUS_TIMEOUT];
+	var map_type = [BMAP_NORMAL_MAP, BMAP_PERSPECTIVE_MAP, BMAP_SATELLITE_MAP, BMAP_HYBRID_MAP]
+	var navigation_control_obj = {
+		// anchor: 1,
+		// offset: new BMap.Size(100, 100),
+		// type: 2,
+		// showZoomInfo: false,
+		// enableGeolacation: true,
+	}
+	var scale_control_obj = {
+		// anchor: 0,
+		// offset: new BMap.Size(10, 10),
+	}
+
+	var copyright_control_obj = {
+		anchor: 3,
+		offset: new BMap.Size(10, 10),
+	}
+
+	var overview_map_control_obj = {
+		// anchor: 2,
+		// offset: new BMap.Size(10, 50),
+		// size: new BMap.Size(300, 300),
+		// isOpen: true,
+	}
+
+	var map_type_control_obj = {
+		// type: BMAP_MAPTYPE_CONTROL_MAP,
+		// maptype: map_type,
+	}
+
+	var geolocation_control_obj = {
+		// anchor: 3,
+		// offset: new BMap.Size(10, 10),
+		// showAddressBar: false,
+		// enableAutoLocation: true,
+		// // locationIcon: Icon
+	}
+
+
+	var map_type_control = new BMap.MapTypeControl(map_type_control_obj);
+	var overview_map_control = new BMap.OverviewMapControl(overview_map_control_obj);
+	var navigation_control = new BMap.NavigationControl(navigation_control_obj);
+	var copyright_control = new BMap.CopyrightControl(copyright_control_obj);
+	var scale_control = new BMap.ScaleControl(scale_control_obj);
+	var geolocation_control = new BMap.GeolocationControl(geolocation_control_obj);
 	var panorama_control = new BMap.PanoramaControl();
 
+	scale_control.setUnit(BMAP_UNIT_IMPERIAL)
+	console.log(scale_control.getUnit())
+	scale_control.setUnit(BMAP_UNIT_METRIC)
+	console.log(scale_control.getUnit())
+	var copyright = {
+		id: 1,
+		content: "我的版权信息",
+		bounds: new BMap.Bounds(new BMap.Point(102.424067, 24.714754), new BMap.Point(103.032328, 25.215284)),
+	}
+	var copyright_1 = {
+		id: 2,
+		content: "author by ch51ff",
+		bounds: new BMap.Bounds(new BMap.Point(102.424067, 24.714754), new BMap.Point(103.032328, 25.215284)),
+	}
+
+	copyright_control.addCopyright(copyright);
+	copyright_control.addCopyright(copyright_1);
+
+	map.addControl(copyright_control)
+
+
+	console.log(copyright_control.getCopyright(1));
+	console.log(copyright_control.getCopyrightCollection());
+	copyright_control.removeCopyright(1)
+	console.log(copyright_control.getCopyrightCollection());
+
 	map.centerAndZoom(new BMap.Point(102.707092, 25.002890), 25);
-	map.addControl(map_type_control);
 	map.setCurrentCity("昆明");
+
+	$(".control-panel").css({left: "100px"});
 
 	var $menu = $("#menu"), $m_items = $(".item", $menu);
 	$m_items.each(function() {
--- public/js/bmap/control_new.js
+++ public/js/bmap/control_new.js
@@ -6,13 +6,46 @@
 	map.setCurrentCity("昆明");
 	map.enableScrollWheelZoom();
 
-	var type = [BMAP_NORMAL_MAP, BMAP_PERSPECTIVE_MAP, BMAP_SATELLITE_MAP, BMAP_HYBRID_MAP], active_type = 0;
+	var anchor = [BMAP_ANCHOR_TOP_LEFT, BMAP_ANCHOR_TOP_RIGHT, BMAP_ANCHOR_BOTTOM_LEFT, BMAP_ANCHOR_BOTTOM_RIGHT];
 
 	var $menu = $("#menu"), $m_items = $(".item", $menu);
 	$m_items.each(function() {
 		if($(this).hasClass("m-control_new"))
 			$(this).addClass("active");
 	})
+
+	var zoomControl = function() {
+		this.defaultAnchor = BMAP_ANCHOR_BOTTOM_LEFT;
+		this.defaultOffset = new BMap.Size(10, 50);
+	}
+
+	zoomControl.prototype = new BMap.Control();
+	
+	zoomControl.prototype.initialize = function(map) {
+		var div = document.createElement("div");
+		div.className = "zoom_control";
+
+		var e1 = document.createElement("input");
+		e1.type = "button";
+		e1.className = "btn";
+		e1.value = "zoomIn";
+		e1.onclick = function() {
+			map.zoomIn();
+		}
+		var e2 = document.createElement("input");
+		e2.type = "button";
+		e2.className = "btn";
+		e2.value = "zoomOut";
+		e2.onclick = function() {
+			map.zoomOut();
+		}
+		div.appendChild(e1)
+		div.appendChild(e2)
+
+		map.getContainer().appendChild(div);
+
+		return div; 
+	}
 
 	var myControl = function() {
 		this.defaultAnchor = BMAP_ANCHOR_TOP_LEFT;
@@ -30,17 +63,51 @@
 		e1.className = "btn";
 		e1.value = "getAnchor";
 		e1.onclick = function() {
-			getMyAnchor();
+			getZoomAnchor();
 		}
 		var e2 = document.createElement("input");
 		e2.type = "button";
 		e2.className = "btn";
 		e2.value = "getOffset";
 		e2.onclick = function() {
-			getMyOffset()
+			getZoomOffset()
 		}
+		var e3 = document.createElement("input");
+		e3.type = "button";
+		e3.className = "btn";
+		e3.value = "isVisible"
+		e3.onclick = function() {
+			getZoomVisible()
+		}
+		var e4 = document.createElement("input");
+		e4.type = "button";
+		e4.className = "btn";
+		e4.value = "toggleZoom";
+		e4.onclick = function() {
+			toggleZoom();
+		}
+		var e5 = document.createElement("input");
+		e5.type = "button";
+		e5.className = "btn";
+		e5.value = "toggleAnchor";
+		e5.onclick = function() {
+			toggleAnchor();
+		}
+		var e6 = document.createElement("input");
+		e6.type = "button";
+		e6.className = "btn";
+		e6.value = "toggleOffset";
+		e6.onclick = function() {
+			toggleOffset();
+		}
+
+
 		div.appendChild(e1)
 		div.appendChild(e2)
+		div.appendChild(e3)
+		div.appendChild(e4)
+		div.appendChild(e5)
+		div.appendChild(e6)
 
 		map.getContainer().appendChild(div);
 
@@ -48,22 +115,55 @@
 	}
 
 	var my_control = new myControl();
+	var zoom_control = new zoomControl();
 	map.addControl(my_control);
-	// console.log(my_control.getAnchor());
-	// console.log(my_control.getOffset())
-	// console.log(my_control.isVisible())
+	map.addControl(zoom_control);
 
-	var getMyAnchor = function() {
-		var anchor = my_control.getAnchor();
+	var getZoomAnchor = function() {
+		var anchor = zoom_control.getAnchor();
 		console.log(anchor)
 		alert(anchor)
 	}
 
-	var getMyOffset = function() {
-		var offset = my_control.getOffset();
+	var getZoomOffset = function() {
+		var offset = zoom_control.getOffset();
 		console.log(offset)
 		alert("{width:" + offset.width + ", height:" + offset.height + "}")
 	}
+
+	var getZoomVisible = function() {
+		var visible = zoom_control.isVisible();
+		console.log(visible);
+		alert(visible)
+	}
+
+	var toggleZoom = function() {
+		var visible = zoom_control.isVisible();
+		if(visible) {
+			zoom_control.hide();
+		} else {
+			zoom_control.show();
+		}
+	}
+
+	var toggleAnchor = function() {
+		var anchor = zoom_control.getAnchor();
+		if(anchor == 2) {
+			zoom_control.setAnchor(3);
+		} else {
+			zoom_control.setAnchor(2);
+		}
+	}
+
+	var toggleOffset = function() {
+		var offset = zoom_control.getOffset();
+		if(offset.height == 50) {
+			zoom_control.setOffset(new BMap.Size(50, 100))
+		} else {
+			zoom_control.setOffset(new BMap.Size(10, 50))
+		}
+	}
+
 
 	var $control = $("#control"), $exc = $("#exc"), $zoom = $("#zoom");
 })(jQuery)
