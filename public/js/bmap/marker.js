18d2390e7f1d218c2bd58013db23d98fd1328b54
ch51ff <371476909@qq.com>
2016-09-12 15:39:29 +0800

    bmap

--- public/js/bmap/marker.js
+++ public/js/bmap/marker.js
@@ -12,7 +12,24 @@
 	$m_items.each(function() {
 		if($(this).hasClass("m-marker"))
 			$(this).addClass("active");
-	})
+	});
+
+	var marker_obj = {
+		offset: new BMap.Size(0, 0), //位置偏移量
+		// icon: ICON, //图标
+		enableMassClear: true, //是否被clearOVerlays清除
+		enableDragging: true, //是否可拖拽
+		enableClicking: true, // 是否可点击
+		raiseOnDrag: true, //是否开启拖拽完成动画
+		// draggingCursor: cursor, //拖拽样式
+		rotation: 0, //旋转
+		// shadow: ICON, //阴影
+		title: "地图中心", //hover显示标题
+	}
+
+	var marker = new BMap.Marker(new BMap.Point(102.707092, 25.002890), marker_obj);
+
+	map.addOverlay(marker);
 
 	var $control = $("#control"), $exc = $("#exc"), $zoom = $("#zoom");
 })(jQuery)
